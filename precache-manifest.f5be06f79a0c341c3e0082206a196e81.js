4b22456c2007dec9a16104529d9e760a50c854fc
Daniel <allegro.danielsobieraj@gmail.com>
2020-09-20 23:13:29 +0200

    Updates

---
+++ precache-manifest.f5be06f79a0c341c3e0082206a196e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c05a61d674ab9225fab4901070b1ad4",
+    "url": "/RickMortyApp/index.html"
+  },
+  {
+    "revision": "ac2be37ef9cac1845ada",
+    "url": "/RickMortyApp/static/css/main.55844d69.chunk.css"
+  },
+  {
+    "revision": "a5f7cdde99ff47d4f3a3",
+    "url": "/RickMortyApp/static/js/2.9139db23.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/RickMortyApp/static/js/2.9139db23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2be37ef9cac1845ada",
+    "url": "/RickMortyApp/static/js/main.e2048def.chunk.js"
+  },
+  {
+    "revision": "a18b304588a41e114f40",
+    "url": "/RickMortyApp/static/js/runtime-main.3188dba7.js"
+  }
+]);
